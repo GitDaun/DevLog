df9238b2ec28894963e1b3f84b554d55d8e9c681
daun <daunje0@gmail.com>
2025-02-04 13:37:47 +0900

    style: Header.tsx 스타일 수정

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -12,10 +12,11 @@ export default function Header() {
   return (
     <header className='border-gray-200 py-[4dvh] bg-[#f5f5f5] header'>
       <nav>
-        <ul className='font-sour-gummy text-2xl font-[120]  flex flex-row gap-12 justify-center'>
+        <ul className='font-sour-gummy text-2xl font-extralight  flex flex-row space-x-36 justify-center
+        '>
 
           <motion.div 
-            className='flex flex-row gap-1 items-center'
+            className='flex flex-row gap-1 items-center '
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
