e6cb98d88ae94f6da5d5b0ccb5e6c14deeca159b
daun <daunje0@gmail.com>
2025-02-11 23:44:14 +0900

    feat: 헤더 햄버거 메뉴 추가

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,69 +1,110 @@
 'use client'
 
+import { useState } from 'react'
+
 import Link from 'next/link'
 import AboutMe from '../assets/aboutMe.svg'
 import Blog from '../assets/blog.svg'
 import Portfolio from '../assets/portfolio.svg'
-import Contact from '../assets/contact.svg'
+import { motion, AnimatePresence } from 'framer-motion'
+import clsx from 'clsx'
 
-import { motion } from 'framer-motion'
+interface NavItem {
+  icon: any; // SVG 컴포넌트의 정확한 타입을 사용하는 것이 좋습니다
+  text: string;
+  href: string;
+}
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navItems: NavItem[] = [
+    { icon: AboutMe, text: 'About Me', href: '/' },
+    { icon: Blog, text: 'Blog', href: '/blog' },
+    { icon: Portfolio, text: 'Portfolio', href: '/' },
+  ];
+
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className='border-gray-200 py-[4dvh] bg-[#f5f5f5] header'>
-      <nav>
-        <ul className='font-sour-gummy text-2xl font-extralight  flex flex-row space-x-36 justify-center
-        '>
-
-          <motion.div 
-            className='flex flex-row gap-1 items-center '
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-          >
-            <AboutMe className='size-6'/>
-            <li >
-            <Link href="/">About Me</Link>
-            </li>
-          </motion.div>
-
-          <motion.div 
-            className='flex flex-row gap-1 items-center'
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-          >
-            <Blog className='size-6'/>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
-          </motion.div>
-
-          <motion.div 
-            className='flex flex-row gap-1 items-center'
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-          >
-            <Portfolio className='size-6'/>
-            <li>
-              <Link href="/">Portfolio</Link>
-            </li>
-          </motion.div>
-          
-          <motion.div 
-            className='flex flex-row gap-1 items-center'
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-          >
-            <Contact className='size-6'/>
-            <li>
-              <Link href="/">Contact</Link>
-            </li>
-          </motion.div>
+    <header className='border-gray-200 py-[2dvh]'>
+      <nav className='' >
+      <AnimatePresence>
+        <ul 
+          className={clsx(`text-3xl  
+          sm:font-extralight sm:flex sm:flex-row sm:items-center sm:justify-evenly sm:scale-y-100
+        *:hover:text-slate-400 *:flex *:flex-row *:gap-1 *:items-center *:justify-center`,
+          {'flex flex-col h-screen w-screen bg-amber-300 absolute top-0 left-0 items-center justify-center gap-10 scale-y-100 transition-transform duration-300 ease-in-out origin-bottom ': isOpen === true, 
+          'hidden': isOpen === false
+          })}
+        >
+          {navItems.map(({ icon: Icon, text, href }) => (
+            <motion.div 
+              key={text}
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <Icon className='size-6'/>
+              <li>
+                <Link href={href}>{text}</Link>
+              </li>
+            </motion.div>
+          ))}
         </ul>
+        </AnimatePresence>
+        {/* <ul className=' text-2xl hidden sm:font-extralight sm:flex sm:flex-row sm:items-center sm:justify-evenly 
+        *:hover:text-slate-400 *:flex *:flex-row *:gap-1 *:items-center *:justify-center'>
+          {navItems.map(({ icon: Icon, text, href }) => (
+            <motion.div 
+              key={text}
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <Icon className='size-6'/>
+              <li>
+                <Link href={href}>{text}</Link>
+              </li>
+            </motion.div>
+          ))}
+        </ul> */}
+        <div className="flex gap-2 sm:hidden">
+          <button 
+            aria-label="메뉴 열기" 
+            className={
+              clsx('menu-button group z-1 scale-150', 
+              {'hidden': isOpen === true, 'block': isOpen === false})
+            } 
+            onClick={handleMenuClick}
+          >
+            <i className="text-2xl ri-menu-line"></i>
+          </button>
+          <button 
+            aria-label="메뉴 닫기"
+            onClick={handleMenuClick}
+            className={
+              clsx('menu-button group z-1 scale-150', 
+              {'hidden': isOpen === false, 'block': isOpen === true})
+            }
+          >
+            <i className="text-2xl ri-close-line"></i>
+          </button>
+        </div>
+        <div className="flex gap-2 sm:hidden">
+          <button aria-label="메뉴 열기" className="menu-button group" >
+            <i className="text-2xl ri-sun-line"></i>
+          </button>
+          <button aria-label="메뉴 닫기" className="close-button group" >
+           <i className="text-2xl ri-moon-line"></i>
+          </button>
+        </div>
       </nav>
+        {/* <div 
+          className={clsx('bg-red-300 h-full w-full absolute top-0 left-0 sm:hidden', 
+          {'block': isOpen === true, 'hidden': isOpen === false})}>
+        </div> */}
     </header> 
   )
 }
