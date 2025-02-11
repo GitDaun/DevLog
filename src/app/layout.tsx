bfa5d22c1e41755383eac7d338d019ec0999866b
daun <daunje0@gmail.com>
2025-02-11 23:43:27 +0900

    chore: 라이브러리 추가 및 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,15 @@ const sourGummy = Sour_Gummy({
 
 export const metadata: Metadata = {
   title: "Devlog of Daun",
-  description: "프론트엔드 개발자 정다운의 개발 블로그",
+  description: "FE 정다운의 개발 블로그",
+  icons: {
+    icon: [
+      {
+        url: "/favicon16.jpg",
+        type: "image/jpg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -20,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${sourGummy.variable}  antialiased px-[10%]`} >
+      <body className={`${sourGummy.variable}  antialiased p-4`} >
         <Header/>
         {children}
       </body>
