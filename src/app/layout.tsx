33adbe74ca938f7fed8b74dd14c18505c0fec258
daun <daunje0@gmail.com>
2025-03-03 01:45:13 +0900

    fix: cls 개선안 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 const sourGummy = Sour_Gummy({
   subsets: ['latin', 'latin-ext'],
   variable: "--font-sour-gummy",
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -30,6 +31,13 @@ export default function RootLayout({
     <html lang="ko" suppressHydrationWarning>
       <head suppressHydrationWarning>
         <link href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css" rel="stylesheet" />
+        <link
+          rel="preload"
+          href="https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className={`${sourGummy.variable} antialiased p-4 bg-white dark:bg-gray-900 dark:text-white`} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
